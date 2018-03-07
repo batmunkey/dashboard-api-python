69dc362d78d618d70f2a994a66fa1487f743d6ce
Sam Powell <sambpowell@gmail.com>
2018-03-07 16:03:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Cisco Meraki Dashboard API Python 3.6 Module',
   author = 'Shiyue Cheng',
   author_email = 'shiychen@cisco.com',
-  url = 'https://github.com/meraki/dashboard-api-python',
-  download_url = 'https://github.com/meraki/dashboard-api-python/',
+  url = 'https://github.com/batmunkey/dashboard-api-python',
+  download_url = 'https://github.com/batmunkey/dashboard-api-python/',
   keywords = ['meraki', 'dashboard', 'cisco'],
   classifiers = [],
 )
